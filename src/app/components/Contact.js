3b2eec684a784f30902e4d3bf958b34d178b033a
Reka <reka.kovacs1993@gmail.com>
2023-12-12 16:41:48 +0000

    contact layout

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -11,24 +11,24 @@ export default function Contact() {
           height={100}
         />
         <div className="absolute flex-col top-0 left-0 w-full h-full bg-black bg-opacity-80 flex items-center mb-20">
-        <h1 className="text-center text-3xl text-white my-20">Lépjen kapcsolatba cegünk képviselőjével</h1>
+        <h1 className="text-center text-3xl text-white my-20 mt-40">Lépjen kapcsolatba cegünk képviselőjével</h1>
         <div className=" bg-white rounded-lg  flex flex-col justify-center z-5">
-        <div className="flex flex-col justify-center py-10 px-20">
+        <div className="flex flex-col justify-center py-10 px-40">
           
           <div className="text-center justify-center">
-            <h3 className="text-xl mb-5"><b>Mezei Viktor</b></h3>
+            <h3 className="text-2xl mb-5"><b>Mezei Viktor</b></h3>
             <div className="mb-2">
               <span className="mr-2 text-3xl">&#x2709;</span>
-              <span className="text-lg">Email</span>
+              <span className="text-2xl">Email</span>
             </div>
-            <a href="mailto:email@gmail.com" className="text-2xl">
+            <a href="mailto:email@gmail.com" className="text-lg">
               email@gmail.com
             </a>
             <div className="mb-2 mt-10">
               <span className="mr-2 text-lg">&#128222;</span>
-              <span className="text-lg">Phone</span>
+              <span className="text-2xl">Phone</span>
             </div>
-            <p className="text-2xl">+36 70 3413478</p>
+            <p className="text-lg">+36 70 3413478</p>
           </div>
         </div>
       </div>
--- src/app/components/Tudta.js
+++ src/app/components/Tudta.js
@@ -2,73 +2,80 @@ import Image from "next/image";
 
 export default function Tudta() {
   return (
-    <div className="relative w-full md:mt-20 mt-10">
-      <div className="w-full hidden md:block">
-        <Image
-          src="https://cdn.pixabay.com/photo/2023/08/26/13/54/room-8215035_1280.jpg"
-          alt="Interior Photo"
-          layout="responsive"
-          width={1024}
-          height={500}
-        />
-        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-80 flex items-center justify-center">
-          <div className="absolute w-3/4 1/2 bg-white z-5 flex flex-col items-center">
-            <h1 className="px-20 text-center">Az eddig amatőr módon feladott hirdetések ideje lejárt.</h1>
-            <p className="px-20 text-center text-lg">
-              Jelenleg kínálati piac van, vagyis <b>több az eladó ingatlan, mint a vásárló</b>. Ha azt szeretné, hogy az Ön ingatlanát válasszák, akkor hirdetésének ki kell emelkednie a többi közül.
-            </p>
-            <div className="flex mt-10">
-              <div className="mr-20">
-                <h2 className="text-xl mb-5"><b>Ezért van ránk szüksége</b></h2>
-                <ul className="list-disc list-inside px-4">
-                  <li className="mb-2 flex items-center">
-                    <span className="mr-2 text-lg">&#10003;</span>
-                    Profi lakberendező által berendezett lakás
-                  </li>
-                  <li className="mb-2 flex items-center">
-                    <span className="mr-2 text-lg">&#10003;</span>
-                    Átlagból kiemelkedő hirdetési szövegs
-                  </li>
-                  <li className="mb-2 flex items-center">
-                    <span className="mr-2 text-lg">&#10003;</span>
-                    Profi fotó
-                  </li>
-                  <li className="mb-2 flex items-center">
-                    <span className="mr-2 text-lg">&#10003;</span>
-                    Drónfelvétel
-                  </li>
-                  <li className="flex items-center">
-                    <span className="mr-2 text-lg">&#10003;</span>
-                    3D-s ingatlanséta
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className="text-xl mb-5"><b>Ez már sajnos nem elég</b></h2>
-                <ul className="list-none px-4">
-                  <li className="mb-2 flex items-center">
-                    <span className="mr-2 text-lg">&#10005;</span>
-                    Üres, vagy átlagos berendezésű lakás
-                  </li>
-                  <li className="mb-2 flex items-center">
-                    <span className="mr-2 text-lg">&#10005;</span>
-                    Amatőr megfogalmazás
-                  </li>
-                  <li className="mb-2 flex items-center">
-                    <span className="mr-2 text-lg">&#10005;</span>
-                    Telefonos képek
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="flex justify-center mt-12">
-              <button className="border border-darkgrey hover:bg-black hover:bg-opacity-25 text-grey font-bold py-2 px-4 rounded-full transition duration-300">
-                <a href="/#contact">Kérem a Profi Segítséget!</a>
-              </button>
-            </div>
+<div className="relative w-full md:mt-20 mt-10 ">
+  <div className="w-full hidden md:block">
+    <Image
+      src="https://cdn.pixabay.com/photo/2023/08/26/13/54/room-8215035_1280.jpg"
+      alt="Interior Photo"
+      layout="responsive"
+      width={1024}
+      height={500}
+    />
+    <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-80 flex flex-col justify-evenly">
+      <h1 className="px-20 text-center text-3xl text-white">
+        Az eddig amatőr módon feladott hirdetések ideje lejárt!
+      </h1>
+      <div className="mx-40 flex flex-col items-center bg-white px-20 py-10 rounded-lg">
+        <p className="px-20 text-center text-lg">
+          Jelenleg kínálati piac van, vagyis <b>több az eladó ingatlan, mint a vásárló</b>. Ha azt szeretné, hogy az Ön ingatlanát válasszák, akkor hirdetésének ki kell emelkednie a többi közül.
+        </p>
+        <div className="flex mt-10">
+          <div className="mr-20">
+            <h2 className="text-xl mb-5"><b>Ezért van ránk szüksége</b></h2>
+            <ul className="list-disc list-inside px-4">
+              <li className="mb-2 flex items-center">
+                <span className="mr-2 text-lg">&#10003;</span>
+                Profi lakberendező által berendezett lakás
+              </li>
+              <li className="mb-2 flex items-center">
+                <span className="mr-2 text-lg">&#10003;</span>
+                Átlagból kiemelkedő hirdetési szöveg
+              </li>
+              <li className="mb-2 flex items-center">
+                <span className="mr-2 text-lg">&#10003;</span>
+                Profi fotó
+              </li>
+              <li className="mb-2 flex items-center">
+                <span className="mr-2 text-lg">&#10003;</span>
+                Drónfelvétel
+              </li>
+              <li className="mb-2 flex items-center">
+                <span className="mr-2 text-lg">&#10003;</span>
+                3D-s ingatlanséta
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-xl mb-5"><b>Ez már sajnos nem elég</b></h2>
+            <ul className="list-none px-4">
+              <li className="mb-2 flex items-center">
+                <span className="mr-2 text-lg">&#10005;</span>
+                Üres, vagy átlagos berendezésű lakás
+              </li>
+              <li className="mb-2 flex items-center">
+                <span className="mr-2 text-lg">&#10005;</span>
+                Amatőr megfogalmazás
+              </li>
+              <li className="mb-2 flex items-center">
+                <span className="mr-2 text-lg">&#10005;</span>
+                Telefonos képek
+              </li>
+            </ul>
           </div>
         </div>
+
       </div>
+      <div className="flex items-center justify-center mt-10">
+          <button className="border border-white hover:bg-white hover:bg-opacity-50 text-white font-bold py-2 px-4 rounded-full transition duration-300">
+            <a href="/#contact">Szeretném megtalálni álmaim otthonát</a>
+          </button>
+        </div>
+    </div>
+  </div>
+
+
+
+
       {/* Mobile Layout */}
       <div className="md:hidden h-screen">
           <Image

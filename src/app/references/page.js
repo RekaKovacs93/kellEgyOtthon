f017c321adcce22a5cbbbd936ef5968baedfba82
Reka <reka.kovacs1993@gmail.com>
2024-01-09 18:05:25 +0000

    references array

--- src/app/references/page.js
+++ src/app/references/page.js
@@ -1,7 +1,13 @@
 import References from "../components/References"
+import NavBar from "../components/NavBar"
+import Contact from "../components/Contact"
 
 export default function ReferencesPage () {
     return (
+        <div>
+        <NavBar class="flex flex-col items-end md:flex-row md:items-center"></NavBar>
         <References></References>
+        <div className="w-full justify-center" id="contact"><Contact></Contact></div>
+        </div>
     )
 }

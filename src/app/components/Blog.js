3d6cc392a937b488b9c3f124e9ccb44e4a97860c
Reka <reka.kovacs1993@gmail.com>
2024-01-21 17:58:32 +0000

    contact fix

--- src/app/components/Blog.js
+++
@@ -1,7 +0,0 @@
-export default function Blog(){
-    return(
-        <div className="mt-20">
-            <h1 className="text-center text-2xl m-12">BLOG</h1>
-        </div>
-    )
-}
--- src/app/components/Viktor.js
+++ src/app/components/Viktor.js
@@ -71,7 +71,7 @@ dolgozunk majd együtt.</p>
       </div>
         <div className="flex items-center justify-center mt-10">
           <button className="border-2 border-darkgrey hover:bg-black hover:bg-opacity-25 text-grey font-bold py-2 px-4 rounded-full transition duration-300">
-            <a href="/#contact">Bízzon meg bennünket!</a>
+            <a href="./contact">Bízzon meg bennünket!</a>
           </button>
         </div>
       </div>

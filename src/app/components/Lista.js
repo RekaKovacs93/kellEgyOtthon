04bbec00a38c8a1571b9e0f69ae72c83ceb2045d
Reka <reka.kovacs1993@gmail.com>
2024-02-14 15:49:05 +0000

    adat

---
+++ src/app/components/Lista.js
@@ -0,0 +1,10 @@
+
+
+
+export default function Lista (){
+    return (
+    <div className="flex flex-col justify-center items-center mt-40">
+    <iframe width={1500} height={2000} src="https://m-indenkinekkellegyotthon.ingatlan.com/" className='rounded-lg'></iframe>
+    </div>
+    )
+}

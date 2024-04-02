ec22bdbec8cc5ae77541ed7a7b036f748c7274a7
Reka <reka.kovacs1993@gmail.com>
2024-04-02 10:25:31 +0100

    lista

--- src/app/components/Lista.js
+++ src/app/components/Lista.js
@@ -1,10 +1,15 @@
+import Image from "next/image"
+import Link from "next/link"
 
-export default function Lista (){
+
+export default function Lista ({lakasok}){
     return (
     <div className="flex flex-col justify-center items-center mt-40">
     {/* <iframe width={1500} height={2000} src="https://m-indenkinekkellegyotthon.ingatlan.com/" className='rounded-lg'></iframe> */}
     <h1>Tekintse meg kínálatunkat az <a href="https://m-indenkinekkellegyotthon.ingatlan.com/" target="_blank"><b className="text-lg">Ingatlan.com-on &gt;&gt;</b></a></h1>
 
-    </div>
+
+        </div>
+    
     )
 }
--- src/app/kinalat/page.js
+++ src/app/kinalat/page.js
@@ -2,12 +2,220 @@ import NavBar from "../components/NavBar"
 import Contact from "../components/Contact"
 import Lista from "../components/Lista"
 
+import Head from "next/head"
+
+
+
+const lakasok = [
+  {
+    cim: "Mátyás tér, XVI",
+    ker: "XVI",
+    ar: "20.5M",
+    szobak: 4,
+    alapterulet: "149",
+    telekterulet: "1200",
+    kepek: [
+      "/Matyas09-min.jpg",
+      "/Matyas06-min.jpg",
+      "/Matyas10-min.jpg",
+      "/Matyas12-min.jpg",
+      "/Matyas04-min.jpg",
+      "/Matyas11-min.jpg",
+      "/Matyas05-min.jpg",
+      "/Matyas02-min.jpg",
+      "/Matyas08-min.jpg",
+      "/Matyas01-min.jpg",
+      "/Matyas03-min.jpg",
+      "/Matyas07-min.jpg"
+    ],
+    seta: "https://ths.li/PSWyMGc",
+  
+  },
+  {
+    cim: "Mátyás tér, XVI",
+    ker: "XVI",
+    ar: "20.5M",
+    szobak: 4,
+    alapterulet: "149",
+    telekterulet: "1200",
+    kepek: [
+      "/Matyas09-min.jpg",
+      "/Matyas06-min.jpg",
+      "/Matyas10-min.jpg",
+      "/Matyas12-min.jpg",
+      "/Matyas04-min.jpg",
+      "/Matyas11-min.jpg",
+      "/Matyas05-min.jpg",
+      "/Matyas02-min.jpg",
+      "/Matyas08-min.jpg",
+      "/Matyas01-min.jpg",
+      "/Matyas03-min.jpg",
+      "/Matyas07-min.jpg"
+    ],
+    seta: "https://ths.li/PSWyMGc",
+  
+  },
+  {
+    cim: "Mátyás tér, XVI",
+    ker: "XVI",
+    ar: "20.5M",
+    szobak: 4,
+    alapterulet: "149",
+    telekterulet: "1200",
+    kepek: [
+      "/Matyas09-min.jpg",
+      "/Matyas06-min.jpg",
+      "/Matyas10-min.jpg",
+      "/Matyas12-min.jpg",
+      "/Matyas04-min.jpg",
+      "/Matyas11-min.jpg",
+      "/Matyas05-min.jpg",
+      "/Matyas02-min.jpg",
+      "/Matyas08-min.jpg",
+      "/Matyas01-min.jpg",
+      "/Matyas03-min.jpg",
+      "/Matyas07-min.jpg"
+    ],
+    seta: "https://ths.li/PSWyMGc",
+  
+  },
+  {
+    cim: "Mátyás tér, XVI",
+    ker: "XVI",
+    ar: "20.5M",
+    szobak: 4,
+    alapterulet: "149",
+    telekterulet: "1200",
+    kepek: [
+      "/Matyas09-min.jpg",
+      "/Matyas06-min.jpg",
+      "/Matyas10-min.jpg",
+      "/Matyas12-min.jpg",
+      "/Matyas04-min.jpg",
+      "/Matyas11-min.jpg",
+      "/Matyas05-min.jpg",
+      "/Matyas02-min.jpg",
+      "/Matyas08-min.jpg",
+      "/Matyas01-min.jpg",
+      "/Matyas03-min.jpg",
+      "/Matyas07-min.jpg"
+    ],
+    seta: "https://ths.li/PSWyMGc",
+  
+  },
+  {
+    cim: "Mátyás tér, XVI",
+    ker: "XVI",
+    ar: "20.5M",
+    szobak: 4,
+    alapterulet: "149",
+    telekterulet: "1200",
+    kepek: [
+      "/Matyas09-min.jpg",
+      "/Matyas06-min.jpg",
+      "/Matyas10-min.jpg",
+      "/Matyas12-min.jpg",
+      "/Matyas04-min.jpg",
+      "/Matyas11-min.jpg",
+      "/Matyas05-min.jpg",
+      "/Matyas02-min.jpg",
+      "/Matyas08-min.jpg",
+      "/Matyas01-min.jpg",
+      "/Matyas03-min.jpg",
+      "/Matyas07-min.jpg"
+    ],
+    seta: "https://ths.li/PSWyMGc",
+  
+  },
+  {
+    cim: "Mátyás tér, XVI",
+    ker: "XVI",
+    ar: "20.5M",
+    szobak: 4,
+    alapterulet: "149",
+    telekterulet: "1200",
+    kepek: [
+      "/Matyas09-min.jpg",
+      "/Matyas06-min.jpg",
+      "/Matyas10-min.jpg",
+      "/Matyas12-min.jpg",
+      "/Matyas04-min.jpg",
+      "/Matyas11-min.jpg",
+      "/Matyas05-min.jpg",
+      "/Matyas02-min.jpg",
+      "/Matyas08-min.jpg",
+      "/Matyas01-min.jpg",
+      "/Matyas03-min.jpg",
+      "/Matyas07-min.jpg"
+    ],
+    seta: "https://ths.li/PSWyMGc",
+  
+  },
+  {
+    cim: "Mátyás tér, XVI",
+    ker: "XVI",
+    ar: "20.5M",
+    szobak: 4,
+    alapterulet: "149",
+    telekterulet: "1200",
+    kepek: [
+      "/Matyas09-min.jpg",
+      "/Matyas06-min.jpg",
+      "/Matyas10-min.jpg",
+      "/Matyas12-min.jpg",
+      "/Matyas04-min.jpg",
+      "/Matyas11-min.jpg",
+      "/Matyas05-min.jpg",
+      "/Matyas02-min.jpg",
+      "/Matyas08-min.jpg",
+      "/Matyas01-min.jpg",
+      "/Matyas03-min.jpg",
+      "/Matyas07-min.jpg"
+    ],
+    seta: "https://ths.li/PSWyMGc",
+  
+  },
+  {
+    cim: "Mátyás tér, XVI",
+    ker: "XVI",
+    ar: "20.5M",
+    szobak: 4,
+    alapterulet: "149",
+    telekterulet: "1200",
+    kepek: [
+      "/Matyas09-min.jpg",
+      "/Matyas06-min.jpg",
+      "/Matyas10-min.jpg",
+      "/Matyas12-min.jpg",
+      "/Matyas04-min.jpg",
+      "/Matyas11-min.jpg",
+      "/Matyas05-min.jpg",
+      "/Matyas02-min.jpg",
+      "/Matyas08-min.jpg",
+      "/Matyas01-min.jpg",
+      "/Matyas03-min.jpg",
+      "/Matyas07-min.jpg"
+    ],
+    seta: "https://ths.li/PSWyMGc",
+  
+  }
+
+]
+
 export default function KinalatPage() {
   return (
     <main className="flex min-h-screen flex-col items-center">
 
+        <Head>
+        
+        {lakasok.map((lakas) => (
+          lakas.kepek.map((image, index) => (
+          <link key={index} rel="preload" href={image} as="image" />
+        ))))}
+        </Head> 
+
         <NavBar class="flex flex-col items-end md:flex-row md:items-center"></NavBar>
-        <Lista></Lista>
+        <Lista lakasok={lakasok}></Lista>
         <div className="w-full justify-center" id="contact"><Contact></Contact></div>
 
     </main>
